731c8ba9f414934877f11c133d2da4d2bc7442dd
matheushnascimento <matheus.mhddn@gmail.com>
2023-11-16 14:10:58 -0300

    lógica de troca de páginas

--- js/router.js
+++ js/router.js
@@ -1,3 +1,26 @@
 export class Router {
-  
+  routes = {};
+
+  add(routname, page) {
+    this.routes[routname] = page;
+  }
+
+  route(ev) {
+    ev = ev || window.event;
+    ev.preventDefault();
+
+    window.history.pushState({}, "", ev.target.href);
+
+    this.handle();
+  }
+
+  handle() {
+    const { pathname } = window.location;
+    const route = this.routes[pathname] || this.routes[404];
+    fetch(route)
+      .then(data => data.text())
+      .then(html => {
+        document.querySelector("main").innerHTML = html;
+      });
+  }
 }
